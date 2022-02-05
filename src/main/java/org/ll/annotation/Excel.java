da3abe781caabb7bdadee4b3d9c730f239be2a2a
ll <myrosicky@163.com>
2022-02-05 22:21:35 +0800

    add excel annotation

---
+++ src/main/java/org/ll/annotation/Excel.java
@@ -0,0 +1,16 @@
+package org.ll.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({METHOD, FIELD})
+public @interface Excel {
+    String value();
+}
---
+++ src/main/java/org/ll/processor/ExcelProcessor.java
@@ -0,0 +1,45 @@
+package org.ll.processor;
+
+import com.sun.rowset.internal.Row;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Set;
+
+@SupportedAnnotationTypes("org.ll.annotation.Excel") @Slf4j
+//@SupportedSourceVersion(SourceVersion.RELEASE_8)
+public class ExcelProcessor extends AbstractProcessor {
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        for ( TypeElement annotation : annotations ) {
+            for ( Element element : roundEnv.getElementsAnnotatedWith(annotation) ) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "found @Log at " + element);
+            }
+        }
+        return true;
+    }
+
+    private void readFile(File f){
+        try(FileInputStream fis = new FileInputStream(f);) {
+            XSSFWorkbook sheets = new XSSFWorkbook(fis);
+            Sheet sheet1 = sheets.getSheetAt(0);
+            for (int i = 1; i < sheet1.getLastRowNum() + 1; i++) {
+                Row row1 = sheet1.getRow(i);
+                System.out.println(row1.getCell(0).getStringCellValue() + "   " + row1.getCell(1).getStringCellValue() + "   " + row1.getCell(2).getStringCellValue());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
